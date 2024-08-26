56304f78c275304a942abb9f1b9208f3ee5f515c
William Frank Monroy Mamani <william_monroy777@hotmail.com>
2024-08-25 18:33:21 -0600

    refactor: 💡 Rename

--- quiz3.py
+++ quiz3.py
@@ -1,59 +1,44 @@
 import numpy as np
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f'({self.x}, {self.y})'
-
 def read_points_from_file(file_path):
-    points = np.array([])
-    M = 0
-    k = 0
-
     with open(file_path, 'r') as file:
-        # Read the first line to get the number of points and columns
         first_line = file.readline().strip().split()
         M = int(first_line[1])
         k = int(first_line[2])
 
-        # Read the remaining lines to get the points
-        for _ in range(M):
-            line = file.readline().strip().split()
-            x, y = map(float, line)
-            point = Point(x, y)
-            points = np.append(points, point)
-
+        points = np.loadtxt(file, delimiter=' ')
+    
     return M, k, points
 
-def distance(p1, p2):
-    return np.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2)
-
-def calculate_u(points, alfa, M):
-    u = 0
-    for i in range(M - 1):
-        for j in range(M - 1):
-            if i != j:
-                d = distance(points[i], points[j])
-                u += 1 / (d ** alfa)
+def calculate_u(points, alfa):
+    # Vectorized distance computation
+    dist_matrix = np.sqrt(np.sum((points[:, np.newaxis, :] - points[np.newaxis, :, :]) ** 2, axis=-1))
+    np.fill_diagonal(dist_matrix, np.inf)  # Avoid division by zero
+    u = np.sum(1 / (dist_matrix ** alfa))
     return u
 
 # Usage example
-file_path = './ZCAT2_1000_02D.pof'
+file_path = 'ZCAT2_1000_02D.pof'
 M, k, points = read_points_from_file(file_path)
 alfa = k + 1
+u = calculate_u(points, alfa)
 
 while M > 100:
-    u = calculate_u(points, alfa, M)
+    
+    print("u: ", u)
     C = []
 
-    for i in range(M - 1):
-        temp_points = np.delete(points, i)
-        temp_c = u - calculate_u(temp_points, alfa, M)
+    # Precompute distances and u values for optimization
+    for i in range(M):
+        temp_points = np.delete(points, i, axis=0)
+        temp_u = calculate_u(temp_points, alfa)
+        temp_c = u - temp_u
         C.append(temp_c)
+    
     max_index = np.argmax(C)
-    points = np.delete(points, max_index)
-    M = M - 1
+    u -= C[max_index]
+    print(max_index)
+    points = np.delete(points, max_index, axis=0)
+    M -= 1
+
 print(points)
